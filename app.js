854c752ba7ac13d731e0c2301dc1fd6e1a6c8248
areezalamin <alaminmillat24434@gmail.com>
2022-06-13 21:34:34 +0600

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 document.getElementById('for-nagative').addEventListener('click', function(){
     const forNegativeNumber = document.getElementById('for-number')
     const forNegativeNumberValue = forNegativeNumber.value ;
-    const forNegativeNumberValueChange = parseFloat(forNegativeNumberValue)
+    const forNegativeNumberValueChange = parseInt(forNegativeNumberValue)
     const forDecreaseNegativeNumberValue = forNegativeNumberValueChange - 1
-    forNegativeNumber.value = parseFloat(forDecreaseNegativeNumberValue)
+    forNegativeNumber.value = parseInt(forDecreaseNegativeNumberValue);
+
+
+    const totalCost = document.getElementById('total-cost')
+    const totalCostValue = totalCost.innerText
+    const totalCostValueChange = parseInt(totalCostValue)
+    const totalCostValueFinal = totalCostValueChange - 1219;
+    totalCost.innerText = totalCostValueFinal;
+
+
+    const subTotalCost = document.getElementById('sub-total')
+    const subTotalCostValue = subTotalCost.innerText
+    const subTotalCostValueChange = parseInt(subTotalCostValue)
+    const subTotalCostValueFinal = subTotalCostValueChange - 1219;
+    subTotalCost.innerText = subTotalCostValueFinal;
 })
 
 document.getElementById('for-positive').addEventListener('click', function(){
 
     const forPositiveNumber = document.getElementById('for-number')
     const forPositiveNumberValue = forPositiveNumber.value ;
-    const forPositiveNumberValueChange = parseFloat(forPositiveNumberValue)
+    const forPositiveNumberValueChange = parseInt(forPositiveNumberValue)
     const forIncreasePositiveNumberValue = forPositiveNumberValueChange + 1;
-    forPositiveNumber.value = parseFloat(forIncreasePositiveNumberValue);
+    forPositiveNumber.value = parseInt(forIncreasePositiveNumberValue);
+
+
+    const totalCost = document.getElementById('total-cost')
+    const totalCostValue = totalCost.innerText
+    const totalCostValueChange = parseInt(totalCostValue)
+    const totalCostValueFinal = totalCostValueChange + 1219;
+    totalCost.innerText = totalCostValueFinal;
+
+
+    const subTotalCost = document.getElementById('sub-total')
+    const subTotalCostValue = subTotalCost.innerText
+    const subTotalCostValueChange = parseInt(subTotalCostValue)
+    const subTotalCostValueFinal = subTotalCostValueChange + 1219;
+    subTotalCost.innerText = subTotalCostValueFinal;
 
 })
